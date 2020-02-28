393dbb41fb5b7778fe3c888170e689f0b7bdfb38
Kingsley <k@n.com>
2020-02-28 10:05:31 +0100

    Created task and readme

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# Task
+# Template literals drill
 
+Practice converting double/single quoted strings to template literals (backtick strings).  Template literals allow variables and function calls to be made directly inside the string, instead of having to break the string up.  They also allow multiline strings.
 
-
-**Note:** Styles are provided for you in master.css. Look in the file to find the approriate class names to use on your HTML elements.
-
-### Example
-
-[The example is here](https://link).
-
-## Task
-
-Clone or download this repository onto your computer.  You will start out in the "master" branch which contains an empty project.
-
-Try to recreate the website above.  Firstly, try to create it without any help.  If you are unsure of what to do, you can follow the steps below.  If the steps don't help, checkout out the "answer" branch from this repository.  The answer branch contains a working example.
-
-## Steps
-
-1. 
+1. Questions are inside master.js,
+1. Each question has a variable called `string` that is the original string, and a variable called `literal`,
+1. You need to create literal variable using template literal style.  The literal you create should output the same thing as the original string.
+1. You should not change the original string, or any of the variables used.  The only thing you will set is the template literal variable.
+1. In the browser console, you will see the original string and the template literal output, so you can check whether they output the same thing or not.
+1. **Note:** some of the questions have been done for you as an example of the following questions
---
+++ js/master.js
@@ -0,0 +1,220 @@
+/*** Dont touch */
+let questionNumber = 0;
+let string;
+let literal;
+/*** Dont touch */
+
+
+// question 1
+
+string = "Hello";
+
+const name = "Bob";
+string = "Hello "+name;
+literal = `Hello ${name}`;
+
+answer(string, literal);
+
+
+// question 2
+
+string = "Multiline\n"
+    + "string";
+literal = `Multiline
+string`;
+
+answer(string, literal);
+
+
+// question 3
+
+string = "123 Fake street\n"
+    + "Hackney\n"
+    + "London\n"
+    + "United Kingdom\n";
+    + "SN1 7UP";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 4
+
+// HTML
+
+const title = "Noroff";
+string = "<h1>" + title + "</h1>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 5
+
+const paragraph = "In auctor lobortis lacus. Phasellus gravida semper nisi. Fusce egestas elit eget lorem.";
+string = "<p>" + paragraph + "</p>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 6
+
+const numberOfLikes = 10;
+string = "<div>Number of likes: " + numberOfLikes + "</div>";
+
+literal = ``;
+
+answer(string, literal);
+
+
+// question 7
+
+const car = {
+    model: "Fiat",
+    price: "500000",
+};
+string = "<p>" + car.model + " for sale</p>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 8
+
+string = "<p>" + car.model + " for sale for " + car.price + "</p>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 9
+
+const user = {
+    firstName: "Bob",
+    lastName: "Svensson"
+};
+string = "<p>Welcome back " + user.firstName + " " + user.lastName + "</p>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 10
+
+const article = {
+    title: "Latest news",
+    intro: "Quisque malesuada placerat nisl. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Curabitur ullamcorper ultricies nisi. Quisque malesuada placerat nisl.",
+    author: "Bob Stevenson",
+}
+string = "<h1>" + article.title + "</h1>\n"
+    + "<div>Writtem by " + article.author + "</div>\n"
+    + "<p>" + article.intro + "</p>"
+literal = ``;
+
+answer(string, literal);
+
+
+// question 11
+
+const width = 10;
+const height = 30;
+string = "<div>Dimensions: " + (width * height) + " meters squared</div>";
+literal = `<div>Dimensions: ${width * height} meters squared</div>`
+
+answer(string, literal);
+
+const adults = 10;
+const children = 30;
+string = "<div>Total people: " + (adults + children) + "</div>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 12
+
+const money = 3000;
+const spent = 1000;
+string = "<div>Remaining money: " + (money - spent) + "</div>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 13
+
+function getName() {
+    return "Bob";
+}
+
+string = "<div>Name: " + getName() + "</div>";
+literal = `<div>Name: ${getName()}</div>`
+
+answer(string, literal);
+
+
+// question 14
+
+function getDate() {
+    return "12/03/2021";
+}
+
+string = "<div>Date: " + getDate() + "</div>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 15
+
+function numberOfAdults() {
+    return 10;
+}
+function numberOfChildren() {
+    return 30;
+}
+
+string = "<div>Total people: " + (numberOfAdults() + numberOfChildren()) + "</div>";
+literal = `<div>Total people: ${numberOfAdults() + numberOfChildren()}</div>`;
+
+answer(string, literal);
+
+
+// question 16
+
+function getWidth() {
+    return 10;
+}
+function getHeight() {
+    return 30;
+}
+
+string = "<div>Size: " + (getWidth() + getHeight()) + " meters squared</div>";
+literal = ``;
+
+answer(string, literal);
+
+
+// question 17
+
+function getPanelContent(title) {
+    return `<h2>${title}</h2>
+    <p>This is a paragraph</p>`;
+}
+
+string = "<div>" + getPanelContent("Welcome") + "</div>";
+literal = ``;
+
+answer(string, literal);
+
+
+
+
+/***** DONT TOUCH THIS *****/
+
+function answer(string, literal) {
+    console.log("----- question " + questionNumber++ + " -----");
+    console.log("string:\n\"" + string + "\"");
+    console.log("literal:\n`" + literal + "`");
+}
